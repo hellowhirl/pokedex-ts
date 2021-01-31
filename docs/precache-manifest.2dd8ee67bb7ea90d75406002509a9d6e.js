a24b7520b7d33acb296113c159e420ddb6b6a2f7
Anthony Avila <anthony.p.avila@gmail.com>
2021-01-31 13:01:26 +0900

    create production build for github pages

---
+++ docs/precache-manifest.2dd8ee67bb7ea90d75406002509a9d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0c18fb852ecc991197134ff23ab1f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e0f7e69135d29f0c378",
+    "url": "/static/css/main.bf1b3ffa.chunk.css"
+  },
+  {
+    "revision": "398998b73493c966542c",
+    "url": "/static/js/2.9d27aa15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d27aa15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0f7e69135d29f0c378",
+    "url": "/static/js/main.c3b02389.chunk.js"
+  },
+  {
+    "revision": "a01a4ec5c2a54779b482",
+    "url": "/static/js/runtime-main.d8bda038.js"
+  }
+]);
